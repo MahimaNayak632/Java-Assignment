8d3c6cdf1f810f933c67b5d7768e19d19e6fc956
Mahima Nayak <nayakmahima632@gmail.com>
2025-05-23 20:19:13 +0530

    Update test.sql

--- 9B.QueryCoffeeByD/test.sql
+++ 9B.QueryCoffeeByD/test.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+CREATE TABLE IF NOT EXISTS coffee (
+    id INT PRIMARY KEY,
+    coffee_name VARCHAR(100),
+    price DOUBLE
+);
+
+-- Insert sample data
+INSERT INTO coffee VALUES
+(1, 'Dalgona', 130.0),
+(2, 'Latte', 120.0),
+(3, 'Drip Coffee', 110.0),
+(4, 'Espresso', 100.0);
 
