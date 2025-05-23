41b076e8f8f6f7dc15a34162dccf32039d917628
Mahima Nayak <nayakmahima632@gmail.com>
2025-05-23 20:06:16 +0530

    Update test.sql

--- 8A.CoffeeDataEntry/test.sql
+++ 8A.CoffeeDataEntry/test.sql
@@ -1,8 +1,11 @@
+-- 1. Create the database (if not already created)
+
 CREATE DATABASE IF NOT EXISTS test;
 
+-- 2. Use the database
+   USE test;
 
-USE test;
-
+-- 3. Create the table Emp
 
 CREATE TABLE IF NOT EXISTS coffee (
     id INT PRIMARY KEY,
